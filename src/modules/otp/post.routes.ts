2e43c1126afeb64d5ebed8a5c19de68b04fa16c1
Devan P S <devanps212@gmail.com>
2026-07-16 15:44:16 +0530

    error fixed

--- src/modules/otp/post.routes.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getPostsByUser, uploadPost } from "../post/post.controller";
-
-const router = express.Router();
-
-router.route("/").post(uploadPost).get(getPostsByUser);
